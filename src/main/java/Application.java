76cda33e787367adae5cf5045468cabc320d1ec9
Spencer Gibb <spencer@gibb.us>
2014-10-20 11:59:31 -0600

    Added @EnableHystrixDashboard which alone is broken;  added @EnableEurekaClient to fix it.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,31 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Configuration
+@ComponentScan
 @EnableAutoConfiguration
+@EnableHystrixDashboard
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+}
+
+@Controller
+class HystrixDashboardController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "forward:/hystrix/index.html";
+    }
+
 }
