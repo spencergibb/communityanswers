a7094641c2f5a76f649119922378affcc8c651ad
Spencer Gibb <spencer@gibb.us>
2014-10-30 14:22:50 -0600

    remove @EnableEurekaClient

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,5 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @ComponentScan
 @EnableAutoConfiguration
 @EnableHystrixDashboard
-@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
