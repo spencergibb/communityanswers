26ebc735e152f665c9c678cc9695869700ec7201
Spencer Gibb <spencer@gibb.us>
2014-11-20 10:34:36 -0700

    added example code from so27040684

--- src/main/java/ZuulServer.java
+++ src/main/java/ZuulServer.java
@@ -1,13 +1,16 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 @EnableAutoConfiguration
-public class Application {
+@EnableZuulProxy
+public class ZuulServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ZuulServer.class, args);
     }
 }
