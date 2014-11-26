479c6ac2b5256c906b5d00a74fc2583adf103404
Spencer Gibb <spencer@gibb.us>
2014-11-26 13:54:50 -0700

    server yml file in configserver instead of git

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,11 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
+@EnableConfigServer
 public class Application {
 
     public static void main(String[] args) {
